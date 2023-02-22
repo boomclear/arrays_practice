2ddc01ecbb481e8d527751b5a8283e13b2c59bc6
Austin Woods <austinwoods99@gmail.com>
2023-02-22 14:04:46 -0500

    Call 4 array methods and explain them in comments

--- arrays.rb
+++ arrays.rb
@@ -2,3 +2,8 @@ words = ["way", "weave", "dodge", "duck"]
 numbers = [1, 2, 3, 4]
 floats = [1.1, 2.2, 3.3, 4.4]
 booleans = [true, true, false, false]
+
+words.pop # removes the last element in the array and returns it
+numbers.shift # removes first element in an array and returns it
+floats.unshift(5.5) # puts parameter at the front of an array
+booleans.push(true) # adds element to end of array and returns new length of the array
