e8cea93f68afd1bc27d64587e7223d24a0c9f2d8
Austin Woods <austinwoods99@gmail.com>
2023-02-22 14:08:49 -0500

    Add another array method and explain it.

--- arrays.rb
+++ arrays.rb
@@ -11,3 +11,5 @@ booleans.push(true) # adds element to end of array and returns new length of the
 # index posistion starts counting at 0. The first element of every array is index 0.
 # it then counts forward from there. the second element in an array is index 1 for example. 
 # words[1] is 'weave' for example.
+
+words.length # the length of words is 3 elements. We used pop method above on it so one of the elements was removed. 
